3dbff7d97ca81625c82e91f2fea26868881a7210
kdkhost <kdkhost@users.noreply.github.com>
2026-07-03 03:54:43 -0300

    fix: remove overlay #preloader do CSS, adiciona logo no admin

--- backend/config/adminlte.php
+++ backend/config/adminlte.php
@@ -14,9 +14,9 @@ return [
     ],
 
     'logo' => '<b>Paullada</b> DJ',
-    'logo_img' => null,
+    'logo_img' => '/images/logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
-    'logo_img_xl' => null,
+    'logo_img_xl' => '/images/logo.png',
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'Paullada DJ',
 
